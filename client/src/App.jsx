f9b1ea7418053a54b40d84fb660990e1126d1973
ShariqAnsari88 <36728316+ShariqAnsari88@users.noreply.github.com>
2022-12-30 20:20:39 +0530

    url changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
     const fetchBotResponse = async () => {
         const { data } = await axios.post(
-            "http://localhost:4000",
+            "https://chatgpt-app-3nym.onrender.com",
             { input },
             {
                 headers: {
