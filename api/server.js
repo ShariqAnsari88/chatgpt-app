4f8c1a9e5f908ad47ec3c91f0fc59ba8f75c7c1b
ShariqAnsari88 <36728316+ShariqAnsari88@users.noreply.github.com>
2022-12-30 19:37:53 +0530

    first commit

---
+++ api/server.js
@@ -0,0 +1,46 @@
+import express from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+    res.status(200).send({
+        message: "This is ChatGPT Ai App",
+    });
+});
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+app.post("/", async (req, res) => {
+    try {
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: req.body.input,
+            temperature: 0,
+            max_tokens: 4000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        });
+        console.log("PASSED: ", req.body.input);
+
+        res.status(200).send({
+            bot: response.data.choices[0].text,
+        });
+    } catch (err) {
+        console.log("FAILED: ", req.body.input);
+        console.error(err);
+        res.status(500).send(err);
+    }
+});
+
+app.listen(4000, () => console.log("Server is running on port 4000"));
